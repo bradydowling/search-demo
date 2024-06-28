039e280b81b4743684d8182f0abdb93b46e161ba
Brady Dowling <bradydowling@gmail.com>
2024-06-28 15:54:18 -0400

    Add script to upload chunks

--- scripts/upload_to_trieve.js
+++ scripts/upload_to_trieve.js
@@ -5,22 +5,24 @@ const main = async () => {
   const productChunks = fs.readFileSync("data/products.json");
   const productChunksJSON = JSON.parse(productChunks);
   const productsArray = Object.values(productChunksJSON).flat();
-
-  productsArray.forEach(async (product) => {
-    fetch("https://api.trieve.ai/api/chunk", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "TR-Dataset": process.env.TRIEVE_DATASET_ID,
-        Authorization: process.env.TRIEVE_API_KEY,
-      },
-      body: JSON.stringify({
-        chunk_html: `${product.title}
+  const chunks = productsArray.map((product) => {
+    return {
+      chunk_html: `${product.title}
 ${product.price}
 ${product.details}`,
-        link: product.link,
-      }),
-    });
+      link: product.link,
+      image_urls: [product.image],
+    };
+  });
+
+  fetch("https://api.trieve.ai/api/chunk", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "TR-Dataset": process.env.TRIEVE_DATASET_ID,
+      Authorization: process.env.TRIEVE_API_KEY,
+    },
+    body: JSON.stringify(chunks),
   });
 };
 
